8fabaddb15a3d43f170811726b6f32aa6edcb6ad
ElDav1d <dv>
2025-10-02 09:57:41 +0200

    add customizable CSS

--- src/components/Tabs/Tab/Tab.tsx
+++ src/components/Tabs/Tab/Tab.tsx
@@ -5,17 +5,19 @@ export interface TabProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "pill" | "underline";
   labelText: string;
   isSelected?: boolean;
+  className?: string;
 }
 
 const Tab = ({
   variant = "pill",
   labelText,
   isSelected = false,
+  className = "",
   ...rest
 }: TabProps) => {
   return (
     <button
-      className={`tab-${variant}`}
+      className={`tab-${variant} ${className}`.trim()}
       role="tab"
       aria-selected={isSelected}
       {...rest}
